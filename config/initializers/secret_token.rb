f494244fc4a33c61d9c5d25465be02b59f0153cf
Devi Firdaus <dfedogawa3@gmail.com>
2013-11-28 22:37:40 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hutangdotcom::Application.config.secret_key_base = 'fc0696d64e88674dcfa556147e0711f6e75bae8e1ee835b62c737964748c2b5ac5cec34c56276f8c12976180b4121cd0cec404870877638a99c225bf8092aea4'
